4337a0e9dcbb82bf5a322028111631c1b9b3a94e
Arpit Kushwaha <arpitkushwah01@gmail.com>
2020-06-05 03:01:56 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# chatbot-Nodejs
+# nodejs-chatbot
+Nojde.js chatbot app using api.ai
+
+## First run
+
+```bash
+git clone https://github.com/milenamichalska/nodejs-chatbot.git
+cd nodejs-chatbot
+npm install
+node server.js
+
+```
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var apiai = require('apiai');
+
+var apiai_app = apiai("6d6f546051164cbf9103b51f7f0556d6");
+
+app.use(express.static('public'));
+
+app.get('/index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+})
+
+http.listen(8081,function(){
+    console.log('Listening on localhost:8081');
+})
+
+io.on('connection', function(socket){
+    socket.on('usermsg', function(msg){
+
+        var request = apiai_app.textRequest( msg, {
+            sessionId: 'myfirstbot'
+        });
+
+        request.on('response', function(response) {
+            io.emit('botmsg', response.result.fulfillment.speech);
+        });
+        request.end();
+    });
+});
